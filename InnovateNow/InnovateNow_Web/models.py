f9c049184453c9e6f947d5f8349f27820f2b3dc8
Erick <eric.paredes@duocuc.cl>
2024-07-02 18:34:51 -0400

    modelos

--- InnovateNow/InnovateNow_Web/models.py
+++ InnovateNow/InnovateNow_Web/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Navbar(models.Model):
+    id_navbar = models.AutoField(db_column="idNavbar", primary_key=True)
+    nombre = models.CharField(max_length=50)
+    url = models.CharField(max_length=100)
+
+class quienesSomos(models.Model):
+    id_quienes_somos = models.AutoField(db_column="idQuienesSomos", primary_key=True)
+    titulo = models.CharField(max_length=100)
+    cuerpo = models.TextField()
+
+class Servicio(models.Model):
+    id_servicio = models.AutoField(db_column="idServicio", primary_key=True)
+    nombre_servicio = models.CharField(max_length=50)
+    tipo_servicio = models.CharField(max_length=50)
+    descripcion = models.TextField()
+
+class Testimonios(models.Model):
+    id_testimonio = models.AutoField(db_column="idTestimonio", primary_key=True)
+    nombre_persona = models.CharField(max_length=100)
+    testimonio = models.TextField()
